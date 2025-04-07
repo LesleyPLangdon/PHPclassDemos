cf059d87209688bcc57ae0b8cad4f2da06a5a43e
Lesley Potts-Langdon <61936741+LesleyPLangdon@users.noreply.github.com>
2025-04-07 09:04:24 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+/* DO NOT DELETE THIS COMMENT
+    To run php in codespaces
+    Open terminal
+    Type: php -S localhost:8000
+    Hit enter
+    If it does not open in browser, go to new tab and type URL http://localhost:8000/index.php
+*/
